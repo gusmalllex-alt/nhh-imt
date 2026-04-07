a8620d066cd855ef8d2e37df8fcfe9d92a596366
gusmalllex-alt <gusmall.lex@gmail.com>
2026-04-07 23:21:41 +0700

    feat: add Mr. Suphachai to assignee list

--- src/app/admin/RequestTable.tsx
+++ src/app/admin/RequestTable.tsx
@@ -456,6 +456,7 @@ export default function RequestTable({ initialRequests }: { initialRequests: any
                        <option value="นายเจริญราษฏร์ ลิศรี">นายเจริญราษฏร์ ลิศรี</option>
                        <option value="นายณภัทร ตันสกุล">นายณภัทร ตันสกุล</option>
                        <option value="นายศราวุธ สุขรักษ์">นายศราวุธ สุขรักษ์</option>
+                       <option value="นายศุภชัย สุนารักษ์">นายศุภชัย สุนารักษ์</option>
                     </select>
                     <ChevronDown className="absolute right-6 top-1/2 -translate-y-1/2 w-5 h-5 text-slate-300 pointer-events-none" />
                   </div>
